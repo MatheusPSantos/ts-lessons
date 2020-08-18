0c15864f95b209ba1aba155f6f5b0d5eaed9e26a
matheuspsantos <msantos.2015@alunos.utfpr.edu.br>
2020-08-18 20:13:10 -0300

    single responsabilitie principle

--- src/useCases/createUser/CreateUserUseCase.ts
+++ src/useCases/createUser/CreateUserUseCase.ts
@@ -1,3 +1,19 @@
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { ICreateUserRequestDTO } from "./CreateUserDTO";
+import { User } from "../../entities/User";
+
 export class CreateUserUseCase {
-  async execute() {}
+  constructor(private usersRepository: IUsersRepository) {}
+
+  async execute(data: ICreateUserRequestDTO) {
+    const userAlreadExists = await this.usersRepository.findByEmail(data.email);
+
+    if (userAlreadExists) {
+      throw new Error("User already exists.");
+    }
+
+    const user = new User(data);
+
+    await this.usersRepository.save(user);
+  }
 }
